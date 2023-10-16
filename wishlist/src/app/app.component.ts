a76925a042faff74c2ee5463f3c0b4af7dd42f4f
Bernardo Medina <bernardo.medina@softtek.com>
2023-10-16 09:42:56 -0300

    Making Decisions

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -7,10 +7,10 @@ import { WishItem } from 'src/shared/models/wishItem';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  items= [
-    new WishItem('Learn Angular'),
-    new WishItem('Get coffee',true),
-    new WishItem('Find grass that cut itself'),
+  items:WishItem[]= [
+      new WishItem('Learn Angular'),
+      new WishItem('Get coffee',true),
+      new WishItem('Find grass that cut itself'),
   ]
   title = 'wishlist';
 }
