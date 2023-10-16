210cc532f9da2acb423c89ced3d7ac0102e9ff58
Bernardo Medina <bernardo.medina@softtek.com>
2023-10-16 18:34:57 -0300

    Using ngModel to Bind Values to Form Controls

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -7,15 +7,25 @@ import { WishItem } from 'src/shared/models/wishItem';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  items:WishItem[]= [
-      new WishItem('Learn Angular'),
-      new WishItem('Get coffee',true),
-      new WishItem('Find grass that cut itself'),
-  ]
+  items: WishItem[] = [
+    new WishItem('Learn Angular'),
+    new WishItem('Get coffee', true),
+    new WishItem('Find grass that cut itself'),
+  ];
+
+  newWishText = '';
   title = 'wishlist';
 
+  addNewWish() {
+    if (!this.newWishText.trim())
+      return;
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
 
-  toggleItem(e:Event,item:WishItem) {    
-    item.isComplete=!item.isComplete;
   }
+
+  toggleItem(e: Event, item: WishItem) {
+    item.isComplete = !item.isComplete;
+  }
+
 }
