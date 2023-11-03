02fd8d97cb825d124ae1d5da9819db074218deb9
Bernardo Medina <bernardo.medina@softtek.com>
2023-11-03 07:50:31 -0300

    learned eventEmiter

---
+++ wishlist/src/app/add-wish-form/add-wish-form.component.ts
@@ -0,0 +1,38 @@
+import { Component,Output,EventEmitter,ViewChild, ElementRef } from '@angular/core';
+import { Locales } from 'src/Tools/locales';
+import { WishItem } from 'src/shared/models/wishItem';
+
+@Component({
+  selector: 'app-add-wish-form',
+  templateUrl: './add-wish-form.component.html',
+  styleUrls: ['./add-wish-form.component.scss']
+})
+export class AddWishFormComponent {
+@Output() addWishEmiter=new EventEmitter<WishItem>();
+@ViewChild('theInput') theInput!:ElementRef ;
+locales=Locales;
+newWishText='';
+
+addNewWish=()=> {
+  if (!this.newWishText.trim())
+    return;
+  this.addWishEmiter.emit(new WishItem(this.newWishText))
+  this.newWishText = '';
+}
+textChange(e: string) {
+  //console.log(this.theInput.nativeElement.id);
+}
+inputChange(e:Event) {
+// console.log(e);
+// console.log(e.target);
+
+}
+
+
+keypressHandler(e:KeyboardEvent) {
+ 
+  if(e.key ==='Enter') 
+    this.addNewWish()
+
+}
+}
--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { WishItem } from 'src/shared/models/wishItem';
-import {Locales} from '../Tools/locales'
-let filters=[(x:WishItem)=>true,(x:WishItem)=>!x.isComplete, (x:WishItem)=>x.isComplete];
+import { Locales } from '../Tools/locales'
+let filters = [(x: WishItem) => true, (x: WishItem) => !x.isComplete, (x: WishItem) => x.isComplete];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,43 +9,28 @@ let filters=[(x:WishItem)=>true,(x:WishItem)=>!x.isComplete, (x:WishItem)=>x.isC
 })
 export class AppComponent {
   constructor() {
-    console.log();
+   
   }
-  filterOptions=[{id:0,description:'All'},{id:1,description:'Unfulfilled'},{id:2,description:'Fulfilled'},];
-  locales=Locales;
+  filterOptions = [{ id: 0, description: 'All' }, { id: 1, description: 'Unfulfilled' }, { id: 2, description: 'Fulfilled' },];
+  locales = Locales;
   private items: WishItem[] = [
     new WishItem('Learn Angular'),
     new WishItem('Get coffee', true),
     new WishItem('Find grass that cut itself'),
     new WishItem('Run 3 times a week'),
     new WishItem('Sleep more'),
-    new WishItem('Take vacations',true),    
+    new WishItem('Take vacations', true),
   ];
   listFilter: string = '0';
   newWishText = '';
   title = 'Wish List';
 
-  addNewWish=()=> {
-    if (!this.newWishText.trim())
-      return;
-    this.items.push(new WishItem(this.newWishText));
-    this.newWishText = '';
 
-  }
-   get visibleItems() {
+  get visibleItems() {
     return this.items.filter(filters[+this.listFilter]);
-   }
-
-
-  textChange(e: string) {
-    //console.log(e);
+  }
+  addWish = (wishItem: WishItem) => {
+    this.items.push(wishItem);
   }
 
-
-  keypressHandler(e:KeyboardEvent) {
-   
-    if(e.key ==='Enter') 
-      this.addNewWish()
-
-  }
 }
